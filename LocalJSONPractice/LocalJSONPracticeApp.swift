783f2eba710fc932b8999b5847ad460d225af23d
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2024-04-18 09:05:44 -0400

    Initial Commit

---
+++ LocalJSONPractice/LocalJSONPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalJSONPracticeApp.swift
+//  LocalJSONPractice
+//
+//  Created by Zoe Cutler on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalJSONPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
